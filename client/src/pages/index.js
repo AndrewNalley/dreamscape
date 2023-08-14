527db91b400e8e273f179a8311e798da1a69da8c
AndrewNalley <104536377+AndrewNalley@users.noreply.github.com>
2023-08-14 14:46:50 -0500

    add index to pages

---
+++ client/src/pages/index.js
@@ -0,0 +1,11 @@
+export { default as Audio } from './Audio';
+export { default as Demo } from './Demo';
+export { default as DreamForge } from './DreamForge';
+export { default as HomePage } from './HomePage';
+export { default as Preview } from './Preview';
+export { default as Profile } from './Profile';
+export { default as Signup } from './SignupLogin';
+export { default as Scene } from './Story';
+export { default as StoryWell } from './StoryWell';
+export { default as Text } from './Text';
+export { default as Visual } from './Visual';
