0cc529cf90781be13ac886494aeac39fb7c7d4c3
CaptainFlint1715 <chanseshirley123@gmail.com>
2023-08-14 18:12:18 -0500

    adjusted filenames, imports

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ import {
 
 import { setContext } from "@apollo/client/link/context";
 
-import { Audio, Demo, DreamForge, HomePage, Preview, Profile, Scene, SignUp, StoryWell, Text, Visual } from './pages'
+import { Audio, DreamForge, HomePage, Profile, Scene, SignUp, StoryWell, Text, Visual } from './pages'
 
 const httpLink = createHttpLink({
   uri: "/graphql",
@@ -43,7 +43,7 @@ const router = createBrowserRouter([
   },
 {
   path:"/signUp",
-  element: <Signup/>
+  element: <SignUp/>
 },
 {
   path:"/storyWell",
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,7 +2,7 @@ export { default as Audio } from './Audio';
 export { default as DreamForge } from './DreamForge';
 export { default as HomePage } from './HomePage';
 export { default as Profile } from './Profile';
-export { default as SignUp } from './SignupLogin';
+export { default as SignUp } from './Signup';
 export { default as Scene } from './Story';
 export { default as StoryWell } from './StoryWell';
 export { default as Text } from './Text';
