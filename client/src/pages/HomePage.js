8716fdec5f019bcf65541b15f088fa2b8edcaefd
2Meng <tyan1401@gmail.com>
2023-08-14 13:13:54 -0500

    moved login to signup page renamed signup to signuplogin

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,49 +1,15 @@
 import React, { useState } from 'react';
 import { Link, Navigate } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
-import { LOGIN } from '../utils/mutations';
 
 import Auth from '../utils/auth';
 
 const HomePage = (props) => {
-const [formState, setFormState] = useState({ username: '', password: '' });
-const [login, { error, data }] = useMutation(LOGIN);
 
-// update state based on form input changes
-const handleChange = (event) => {
-  const { name, value } = event.target;
-
-  setFormState({
-    ...formState,
-    [name]: value,
-  });
-};
-
-// submit form
-const handleFormSubmit = async (event) => {
-  event.preventDefault();
-  // console.log(formState);
-  try {
-    const { data } = await login({
-      variables: { ...formState },
-    });
-
-    Auth.login(data.login.token);
-  } catch (error) {
-    console.error(error);
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
   }
-  
-  // clear form values
-  setFormState({
-    username: '',
-    password: '',
-  });
-};
-const logout = (event) => {
-  event.preventDefault();
-  Auth.logout();
-}
-
 
   return (
 
@@ -64,7 +30,7 @@ const logout = (event) => {
                 </>
               ) : (
                 <>
-                  <Link className="btn btn-lg btn-info m-3" to="/signUp">
+                  <Link className="btn btn-lg btn-info m-3" to="/SignupLogin">
                     Sign-up
                   </Link>
                   </>
@@ -84,7 +50,7 @@ const logout = (event) => {
           Imagine escaping to a tranquil paradise, where the worries of everyday life fade away and serenity takes over. Welcome to Dreamsacpe, a place where you can truly unwind, recharge, and find inner peace amidst the beauty of nature.</p> */}
 
 
-<Link className='px-1' to="/signUp">New To Dreamscape Sign Up today</Link>
+<Link className='px-1' to="/SignupLogin">New To Dreamscape Sign Up today</Link>
 <Link className='px-1' to="/storyWell">StoryWell</Link>
 <Link className='px-1' to="/visual">Visual</Link>
 <Link className='px-1' to="/text">Text</Link>
@@ -93,48 +59,7 @@ const logout = (event) => {
 <Link className='px-1' to="/Preview">Preview</Link>
 <Link className='px-1' to="/DreamForge">DreamForge</Link>     
 <Link className='px-1' to="/Demo">Demo</Link>
-<Link className='px-1' to="/Audio">Audio</Link>
-
-{data ? (
-              <p>
-                Success! You may now head{  <Navigate to='/Profile' />}
-              
-              </p>
-            ) :(
-
-        <form className=' align-self-center ' onSubmit={handleFormSubmit}>
-          <h3 className="m-3">Login</h3>
-          <p>User name</p>
-          <input
-            className="form-input d-flex flex-column m-3 "
-            placeholder="User name"
-            name="username"
-            type="text"
-            value={formState.username}
-            onChange={handleChange}
-           />
-          <p>Password</p>
-          <input
-            className="form-input m-3"
-            placeholder="******"
-            name="password"
-            type="password" 
-            value={formState.password}
-            onChange={handleChange} />
-
-          <button
-            className="btn btn-block btn-primary"
-            style={{ cursor: 'pointer' }}
-            type="submit"
-          >
-            Login
-          </button>
-     
-
-
-        </form>)}
-        
-                
+<Link className='px-1' to="/Audio">Audio</Link>       
         
         </div>
      
