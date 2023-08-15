561d1ff309055b54c441b996d6bbf0dd60b15f6d
Andrew Nalley <awnalley@gmail.com>
2023-08-15 13:10:35 -0500

    export photos individually or as array

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,26 +1,143 @@
-const cave = 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061066/dreamscapes/cave_a4ukoz.jpg';
-const clouds = 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061066/dreamscapes/clouds_swlkzo.jpg';
-const desert = 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061066/dreamscapes/desert_syeige.jpg';
-const forest = 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061067/dreamscapes/forest_pc9bsm.jpg';
-const galaxy = 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061066/dreamscapes/galaxy_amxrbw.jpg';
-const island = 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061066/dreamscapes/island_aaodoq.jpg';
-const lake = 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061066/dreamscapes/lake_ytzcaa.jpg';
-const mountains = 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061067/dreamscapes/mountains_eldcsy.jpg';
-const ocean = 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061066/dreamscapes/ocean_vnmtq3.jpg';
-const redOcean = 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061066/dreamscapes/redOcean_kggzps.avif';
-const tree = 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061066/dreamscapes/tree_oqwb4k.jpg';
-const winter = 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061067/dreamscapes/winter_rlabhx.jpg';
-const redForest = 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692064946/dreamscapes/redForest_yukjjy.jpg';
-const heaven = 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692064946/dreamscapes/heaven_h4iixn.jpg';
-const winterForest = 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692064946/dreamscapes/winterForest_lork5w.jpg';
-const desertTree = 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692064946/dreamscapes/desertTree_xcm4sg.jpg';
-const mountainCave = 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692064946/dreamscapes/mountainCave_ufdjva.jpg';
-const mountainLake = 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692064946/dreamscapes/mountainLake_mkwlow.jpg';
-const floatingIsland = 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692064946/dreamscapes/floatingIsland_zqxngg.jpg';
+const cave = {
+    id: 1,
+    name: 'Cave',
+    url: 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061066/dreamscapes/cave_a4ukoz.jpg'
+};
 
+const clouds = {
+    id: 2,
+    name: 'Clouds',
+    url: 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061066/dreamscapes/clouds_swlkzo.jpg'
+};
 
+const desert = {
+    id: 3,
+    name: 'Desert',
+    url: 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061066/dreamscapes/desert_syeige.jpg'
+};
 
-const photoArray = {
+const forest = {
+    id: 4,
+    name: 'Forest',
+    url: 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061067/dreamscapes/forest_pc9bsm.jpg'
+};
+
+const galaxy = {
+    id: 5,
+    name: 'Galaxy',
+    url: 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061066/dreamscapes/galaxy_amxrbw.jpg'
+};
+
+const island = {
+    id: 6,
+    name: 'Island',
+    url: 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061066/dreamscapes/island_aaodoq.jpg'
+};
+
+const lake = {
+    id: 7,
+    name: 'Lake',
+    url: 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061066/dreamscapes/lake_ytzcaa.jpg'
+};
+
+const mountains = {
+    id: 8,
+    name: 'Mountains',
+    url: 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061067/dreamscapes/mountains_eldcsy.jpg'
+};
+
+const ocean = {
+    id: 9,
+    name: 'Ocean',
+    url: 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061066/dreamscapes/ocean_vnmtq3.jpg'
+};
+
+const redOcean = {
+    id: 10,
+    name: 'Red Ocean',
+    url: 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061066/dreamscapes/redOcean_kggzps.avif'
+};
+
+const tree = {
+    id: 11,
+    name: 'Tree',
+    url: 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061066/dreamscapes/tree_oqwb4k.jpg'
+};
+
+const winter = {
+    id: 12,
+    name: 'Winter',
+    url: 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061067/dreamscapes/winter_rlabhx.jpg'
+};
+
+const redForest = {
+    id: 13,
+    name: 'Red Forest',
+    url: 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692064946/dreamscapes/redForest_yukjjy.jpg'
+};
+
+const heaven = {
+    id: 14,
+    name: 'Heaven',
+    url: 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692064946/dreamscapes/heaven_h4iixn.jpg'
+};
+
+const winterForest = {
+    id: 15,
+    name: 'Winter Forest',
+    url: 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692064946/dreamscapes/winterForest_lork5w.jpg'
+};
+
+const desertTree = {
+    id: 16,
+    name: 'Desert Tree',
+    url: 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692064946/dreamscapes/desertTree_xcm4sg.jpg'
+};
+
+const mountainCave = {
+    id: 17,
+    name: 'Mountain Cave',
+    url: 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692064946/dreamscapes/mountainCave_ufdjva.jpg'
+};
+
+const mountainLake = {
+    id: 18,
+    name: 'Mountain Lake',
+    url: 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692064946/dreamscapes/mountainLake_mkwlow.jpg'
+};
+
+const floatingIsland = {
+    id: 19,
+    name: 'Floating Island',
+    url: 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692064946/dreamscapes/floatingIsland_zqxngg.jpg'
+};
+
+// if the array export is to fussy, we can grab the objects 
+// directly here by referencing the assets folder
+export {
+    cave,
+    clouds,
+    desert,
+    forest,
+    galaxy,
+    island,
+    lake,
+    mountains,
+    ocean,
+    redOcean,
+    tree,
+    winter,
+    redForest,
+    heaven,
+    winterForest,
+    desertTree,
+    mountainCave,
+    mountainLake,
+    floatingIsland
+};
+
+// exported objects as array
+const photoArray = [
     cave, 
     clouds, 
     desert, 
@@ -40,6 +157,6 @@ const photoArray = {
     mountainCave,
     mountainLake,
     floatingIsland
-}
+]
 
 export { photoArray };
