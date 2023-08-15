a4c91133c292f55ba5c793e1d5af7d74fe619094
AndrewNalley <104536377+AndrewNalley@users.noreply.github.com>
2023-08-14 20:29:09 -0500

    add photo object

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,17 +1,17 @@
-import cave from './images/cave.jpg';
-import clouds from './images/clouds.jpg';
-import desert from './images/desert.jpg';
-import forest from './images/forest.jpg';
-import galaxy from './images/galaxy.jpg';
-import island from './images/island.jpg';
-import lake from './images/lake.jpg';
-import mountains from './images/mountains.jpg';
-import ocean from './images/ocean.jpg';
-import redOcean from './images/redOcean.avif';
-import tree from './images/tree.jpg';
-import winter from './images/winter.jpg';
+const cave = 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061066/dreamscapes/cave_a4ukoz.jpg';
+const clouds = 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061066/dreamscapes/clouds_swlkzo.jpg';
+const desert = 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061066/dreamscapes/desert_syeige.jpg';
+const forest = 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061067/dreamscapes/forest_pc9bsm.jpg';
+const galaxy = 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061066/dreamscapes/galaxy_amxrbw.jpg';
+const island = 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061066/dreamscapes/island_aaodoq.jpg';
+const lake = 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061066/dreamscapes/lake_ytzcaa.jpg';
+const mountains = 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061067/dreamscapes/mountains_eldcsy.jpg';
+const ocean = 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061066/dreamscapes/ocean_vnmtq3.jpg';
+const redOcean = 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061066/dreamscapes/redOcean_kggzps.avif';
+const tree = 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061066/dreamscapes/tree_oqwb4k.jpg';
+const winter = 'https://res.cloudinary.com/dywzrjptt/image/upload/v1692061067/dreamscapes/winter_rlabhx.jpg';
 
-const photoArray = [
+const photoObj = {
     cave, 
     clouds, 
     desert, 
@@ -24,7 +24,6 @@ const photoArray = [
     redOcean, 
     tree, 
     winter
-]
+}
 
-export { photoArray };
-
+export { photoObj };
