19c94cb8ae78b7189b27399f0fd129b2fe10805c
AndrewNalley <104536377+AndrewNalley@users.noreply.github.com>
2023-08-15 19:55:21 -0500

    add two poems to test

---
+++ client/src/assets/poems.js
@@ -0,0 +1,32 @@
+const oldPond = {
+    id: 1,
+    title: 'The Old Pond',
+    author: 'Matsuo Bashō',
+    lines: `
+    An old silent pond
+    
+    A frog jumps into the pond-
+
+    Splash! Silence again.
+    `
+}
+
+const poppyBlooms = {
+    id: 2,
+    title: 'A Poppy Blooms',
+    author: 'Katsushika Hokusai',
+    lines: `
+    I write, erase, rewrite
+    
+    Erase again, and then
+    
+    A poppy blooms.
+    `
+};
+
+poetryArray = [
+oldPond,
+poppyBlooms,
+]
+
+export { poetryArray };
