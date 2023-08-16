5decc52c068a2283af77ac49fb7e42b3490aebea
AndrewNalley <104536377+AndrewNalley@users.noreply.github.com>
2023-08-15 21:14:46 -0500

    finish adding haikus

--- client/src/assets/poems.js
+++ client/src/assets/poems.js
@@ -22,11 +22,333 @@ const poppyBlooms = {
     
     A poppy blooms.
     `
-};
+}
 
-poetryArray = [
-oldPond,
-poppyBlooms,
-]
+const springOcean = {
+    id: 3,
+    title: 'Spring Ocean',
+    author: 'Yosa Buson',
+    lines: `
+    Spring ocean
 
-export { poetryArray };
+    Swaying gently
+    
+    All day long.
+    `
+}
+
+const moonlight = {
+    id: 4,
+    title: 'In the Moonlight',
+    author: 'Yosa Buson',
+    lines: `
+    In pale moonlight~
+    
+    the wisteria's scent
+    
+    comes from far away.
+    `
+}
+
+const killingSpider = {
+    id: 5,
+    title: 'After Killing a Spider',
+    author: 'Masaoka Shiki',
+    lines: `
+    After killing
+
+    a spider, how lonely I feel
+    
+    in the cold of night!    
+    `
+}
+
+const windWhispered = {
+    id: 6,
+    title: 'The West Wind Whispered',
+    author: 'R. M. Hansard',
+    lines: `
+    The west wind whispered,
+    
+    And touched the eyelids of spring:
+    
+    Her eyes, Primroses.
+    `
+}
+
+const worldDew = {
+    id: 7,
+    title: "A World Of Dew",
+    author: "Kobayashi Issa",
+    lines: `
+    A world of dew,
+
+    And within every dewdrop
+
+    A world of struggle.
+    `
+  }
+
+  const beholders = {
+    id: 8,
+    title: "The Moon Beholders",
+    author: "Matsuo Bashō",
+    lines: `
+    From time to time
+
+    The clouds give rest
+
+    To the moon-beholders.
+    `
+  }
+  
+  const daySpring = {
+    id: 9,
+    title: "A Day In Spring",
+    author: "Masaoka Shiki",
+    lines: `
+    Toward those short trees
+
+    We saw a hawk descending
+
+    On a day in spring.
+    `
+  }
+
+  const clearDay = {
+    id: 10,
+    title: "The Clear Day",
+    author: "Takahama Kyoshi",
+    lines: `
+    The clear day in the rainy season.
+
+    The madder red of the twilight appeared
+
+    And vanished instantly.
+    `
+  }
+
+  const wintry = {
+    id: 11,
+    title: "Over The Wintry",
+    author: "Natsume Sōseki",
+    lines: `
+    Over the wintry
+
+    Forest, winds howl in rage
+
+    With no leaves to blow.
+    `
+  }
+
+  const tasteRain = {
+    id: 12,
+    title: "The Taste Of Rain",
+    author: "Jack Kerouac",
+    lines: `
+    The taste
+
+    Of rain
+
+    —Why kneel?
+    `
+  }
+
+ const oneCandle = {
+    id: 13,
+    title: "Lighting One Candle",
+    author: "Yosa Buson",
+    lines: `
+    The light of a candle
+
+    Is transferred to another candle—
+
+    Spring twilight.
+    `
+  }
+  
+  const dontWeep = {
+    id: 14,
+    title: "Don't Weep",
+    author: "Kobayashi Issa",
+    lines: `
+    Don't weep, insects -
+
+    Lovers, stars themselves,
+
+    Must part.
+    `
+  }
+
+  const fallenLeaves = {
+    id: 15,
+    title: "Fallen Leaves",
+    author: "Yosa Buson",
+    lines: `
+    Blowing from the west
+
+    Fallen leaves gather
+
+    In the east.
+    `
+  }
+
+  const cicadaCry = {
+    id: 16,
+    title: "The Cicada's Cry",
+    author: "Matsuo Bashō",
+    lines: `
+    In the cicada's cry
+
+    No sign can foretell
+
+    How soon it must die.
+    `
+  }
+
+  const wren = {
+    id: 17,
+    title: "The Wren",
+    author: "Kobayashi Issa",
+    lines: `
+    The wren
+
+    Earns his living
+
+    Noiselessly.
+    `
+  }
+
+  const seclusion = {
+    id: 18,
+    title: "Winter Seclusion",
+    author: "Kobayashi Issa",
+    lines: `
+    Winter seclusion -
+
+    Listening, that evening,
+
+    To the rain in the mountain.
+    `
+  }
+
+  const voicesRise = {
+    id: 19,
+    title: "Voices Rise",
+    author: "Natsume Soseki",
+    lines: `
+    Plum flower temple:
+
+    Voices rise
+
+    From the foothills.
+    `
+  }
+
+  const mountainVillage = {
+    id: 20,
+    title: "A Mountain Village",
+    author: "Masaoka Shiki",
+    lines: `
+    A mountain village
+
+    Under the piled-up snow
+
+    The sound of water.
+    `
+  }
+
+  const twilightRain = {
+    id: 21,
+    title: "The Twilight Rain",
+    author: "Matsuo Bashō",
+    lines: `
+    In the twilight rain
+
+    These brilliant-hued hibiscus -
+
+    A lovely sunset.
+    `
+  }
+
+  const distantMountain = {
+    id: 22,
+    title: "The Distant Mountain",
+    author: "Takahama Kyoshi",
+    lines: `
+    The distant mountain
+
+    Catch the sun.
+
+    The desolate field.
+    `
+  }
+
+  const soundRain = {
+    id: 23,
+    title: "The Sound Of Rain",
+    author: "Hasegawa Kanajo",
+    lines: `
+    The sound of rain.
+
+    The clouds on right-side are
+
+    With the summer moon.
+    `
+  }
+
+  const coolBreeze = {
+    id: 24,
+    title: "The Cool Breeze",
+    author: "Kobayashi Issa",
+    lines: `
+    The cool breeze
+
+    Twisted and crooked,
+
+    Then came here.
+    `
+  }
+
+  const persimmons = {
+    id: 25,
+    title: "Persimmons",
+    author: "Masaoka Shiki",
+    lines: `
+    Consider me
+    As one who loved poetry
+    And persimmons.
+    `
+  }
+
+
+//store all poem objects in array
+const poetryArray = [
+        oldPond,
+        poppyBlooms,
+        springOcean,
+        moonlight,
+        killingSpider,
+        windWhispered,
+        worldDew,
+        beholders,
+        daySpring,
+        clearDay,
+        wintry,
+        tasteRain,
+        oneCandle,
+        dontWeep,
+        fallenLeaves,
+        cicadaCry,
+        wren,
+        seclusion,
+        voicesRise,
+        mountainVillage,
+        twilightRain,
+        distantMountain,
+        soundRain,
+        coolBreeze,
+        persimmons,
+    ]
+
+export { poetryArray }
