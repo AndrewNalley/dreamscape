030fd65f6f88eb0f25cebfca3e0b76a5036b5418
AndrewNalley <104536377+AndrewNalley@users.noreply.github.com>
2023-08-16 16:56:53 -0500

    change db connection name

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/test', {
+mongoose.connect('mongodb://127.0.0.1:27017/dreamscape', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
